ebda834d1a9ff886a031dbcf3b5ef47812942d17
Deepesh Genani <deepeshgenani@gmail.com>
2023-10-20 21:49:06 +0530

    added oauth , nodemailer working fine checked

--- backend/controllers/reminderController.js
+++ backend/controllers/reminderController.js
@@ -98,55 +98,35 @@ exports.setreminder = catchAsyncErrors(async (req, res, next) => {
   try {
     const { medicationName, remindAt } = req.body;
     const user = req.user;
+    "2015-03-25T12:00:00Z+5:30"
 
     if (!user) {
       return next(new ErrorHander("Please log in to access this resource", 404));
     }
-
-    const scheduleDate = new Date(`2023-10-19T0${remindAt}:00:00.000Z`);
-    console.log(scheduleDate, "date");
     const message = `Hello ${user.name},\n\nIt's time to take your medication: ${medicationName}.`;
-
-    // Calculate time difference in milliseconds
-    const timeDifference = scheduleDate - Date.now();
-
-    if (timeDifference < 5000) { // If scheduled time is within 5 seconds
+    // Schedule the job
+    schedule.scheduleJob(`*/1 13  * *`, async () => {
       try {
+        console.log(`Email sent to ${user.email} successfully`);
         await sendEmail({
           email: user.email,
           subject: `Medication Reminder`,
           message,
         });
 
-        console.log(`Email sent to ${user.email} successfully`);
-
+        
+        res.status(201).json({
+          success: true,
+          message: "I ran"
+        })
       } catch (error) {
         console.error('Error sending email notification:', error);
       }
-    } else {
-      // Schedule the job
-      setTimeout(async () => {
-        try {
-          await sendEmail({
-            email: user.email,
-            subject: `Medication Reminder`,
-            message,
-          });
-
-          console.log(`Email sent to ${user.email} successfully`);
-
-        } catch (error) {
-          console.error('Error scheduling email notification:', error);
-        }
-      }, timeDifference);
-    }
-
-    res.status(201).json({
-      success: true,
-      message: 'Task added Successfully',
     });
-
   } catch (error) {
-    next(error);
+
   }
 });
+
+
+
